e31c23354e81d9b2ccd98aebc63630f76209e7a0
Dhanavadh T. Saito <105502355+dhanavadh@users.noreply.github.com>
2025-12-06 01:09:19 +0700

    feat: prep for R2

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,27 +8,35 @@ import (
 )
 
 type Config struct {
-	ServerPort string
-	GinMode    string
-	DBHost     string
-	DBPort     string
-	DBUser     string
-	DBPassword string
-	DBName     string
-	DBSSLMode  string
+	ServerPort        string
+	GinMode           string
+	DBHost            string
+	DBPort            string
+	DBUser            string
+	DBPassword        string
+	DBName            string
+	DBSSLMode         string
+	R2AccountID       string
+	R2AccessKeyID     string
+	R2SecretAccessKey string
+	R2BucketName      string
 }
 
 func Load() *Config {
 	godotenv.Load()
 	return &Config{
-		ServerPort: ":" + getEnv("SERVER_PORT", "8080"),
-		GinMode:    getEnv("GIN_MODE", "debug"),
-		DBHost:     getEnv("DB_HOST", "localhost"),
-		DBPort:     getEnv("DB_PORT", "5432"),
-		DBUser:     getEnv("DB_USER", "postgres"),
-		DBPassword: getEnv("DB_PASSWORD", "postgres"),
-		DBName:     getEnv("DB_NAME", "postgres"),
-		DBSSLMode:  getEnv("DB_SSL_MODE", "disable"),
+		ServerPort:        ":" + getEnv("SERVER_PORT", "8080"),
+		GinMode:           getEnv("GIN_MODE", "debug"),
+		DBHost:            getEnv("DB_HOST", "localhost"),
+		DBPort:            getEnv("DB_PORT", "5432"),
+		DBUser:            getEnv("DB_USER", "postgres"),
+		DBPassword:        getEnv("DB_PASSWORD", "postgres"),
+		DBName:            getEnv("DB_NAME", "postgres"),
+		DBSSLMode:         getEnv("DB_SSL_MODE", "disable"),
+		R2AccountID:       getEnv("R2_ACCOUNT_ID", ""),
+		R2AccessKeyID:     getEnv("R2_ACCESS_KEY_ID", ""),
+		R2SecretAccessKey: getEnv("R2_SECRET_ACCESS_KEY", ""),
+		R2BucketName:      getEnv("R2_BUCKET_NAME", ""),
 	}
 }
 
